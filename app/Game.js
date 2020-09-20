f808b56594042bbed5ff47a8eb64066c8900f7bf
Slawek Jarzynka <irasshaimase@interia.pl>
2020-09-20 21:34:33 +0100

    add methods start and guess

--- app/Game.js
+++ app/Game.js
@@ -1,5 +1,27 @@
 class Game {
+    constructor({
+        lettersWrapper,
+        categoryWrapper,
+        wordWrapper,
+        outputWrapper
+    }) {
+        this.lettersWrapper = lettersWrapper
+        this.categoryWrapper = categoryWrapper
+        this.wordWrapper = wordWrapper
+        this.outputWrapper = outputWrapper
+
+    }
+
+    guess(letter) {
+        console.log(letter)
+    }
     start() {
-        console.log("game start")
+        for (let i = 0; i < 26; i++) {
+            const label = (i + 10).toString(36)
+            const button = document.createElement("button")
+            button.innerHTML = label
+            button.addEventListener("click", () => this.guess(label))
+            this.lettersWrapper.appendChild(button)
+        }
     }
 }
